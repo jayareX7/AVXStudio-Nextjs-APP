1142597844c218941eb42877654ac922bde461b4
Admin <jayarex07@gmail.com>
2020-10-08 18:41:35 -0400

    Added Files

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,54 @@
-import React from 'react';
-import Head from 'next/head';
-import Root from '../components/common/Root';
-import Footer from '../components/common/Footer';
-import SocialMedia from '../components/common/SocialMedia';
-import ExploreBanner from '../components/productAssets/ExploreBanner';
-import HeroSection from '../components/homepage/HeroSection';
-import HomeBanner from '../components/homepage/HomeBanner';
-import CategoryBanner from '../components/homepage/CategoryBanner';
-import ProductsBanner from '../components/homepage/ProductsBanner';
-import '../style/scss/style.scss';
+import matter from 'gray-matter'
+import Layout from '../components/Layout'
+import BlogList from '../components/BlogList'
 
-const Home = () => (
-  <Root transparentHeader={true}>
-    <Head>
-      <title>Home | commerce</title>
-    </Head>
+const Index = props => {
+  return (
+    <Layout
+      pathname="/"
+      siteTitle={props.title}
+      siteDescription={props.description}
+    >
+      <section>
+        <BlogList allBlogs={props.allBlogs} />
+      </section>
+    </Layout>
+  )
+}
 
-    <HeroSection />
-    <HomeBanner />
-    <CategoryBanner />
-    <ProductsBanner />
-    <ExploreBanner />
-    <SocialMedia />
-    <Footer />
-  </Root>
-);
+export default Index
 
-export default Home;
+export async function getStaticProps() {
+  const siteConfig = await import(`../data/config.json`)
+  //get posts & context from folder
+  const posts = (context => {
+    const keys = context.keys()
+    const values = keys.map(context)
+
+    const data = keys.map((key, index) => {
+      // Create slug from filename
+      const slug = key
+        .replace(/^.*[\\\/]/, '')
+        .split('.')
+        .slice(0, -1)
+        .join('.')
+      const value = values[index]
+      // Parse yaml metadata & markdownbody in document
+      const document = matter(value.default)
+      return {
+        frontmatter: document.data,
+        markdownBody: document.content,
+        slug,
+      }
+    })
+    return data
+  })(require.context('../posts', true, /\.md$/))
+
+  return {
+    props: {
+      allBlogs: posts,
+      title: siteConfig.default.title,
+      description: siteConfig.default.description,
+    },
+  }
+}
